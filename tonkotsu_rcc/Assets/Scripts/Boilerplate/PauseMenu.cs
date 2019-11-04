f414a73b586e0677f21d68a155c11c078842bbf5
Benjamin Rose <rose.benjamin@t-online.de>
2019-11-04 22:55:01 +0100

    added basic versions of most Boilerplate Scripts (currently missing Singleton)

---
+++ tonkotsu_rcc/Assets/Scripts/Boilerplate/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void Resume()
+    {
+        GameState.Instance.TryChangeState(GameState.GameStates.Play);
+        gameObject.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        this.gameObject.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        GameState.Instance.TryChangeState(GameState.GameStates.Menu);
+    }
+
+    public void Quit()
+    {
+        GameState.Instance.TryChangeState(GameState.GameStates.Quit);
+    }
+}
---
+++ tonkotsu_rcc/Assets/Scripts/Boilerplate/SceneHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    //This definetly should be less dependend on correct build settings, could be changed to Singleton so we don't need statics / even better would be a different public access
+
+    public static void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public static void LoadCredits()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public static void LoadIntro()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public static void LoadLevelWithIndex(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public static void ReloadCurrentScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+
+    public static void LoadNextScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+}
