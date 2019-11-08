aaf2f5246a69e4c53436e7b5c386bf049dea82f0
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-11-08 21:50:34 +0100

    added SetMaterialFloatOnBeat

---
+++ tonkotsu_rcc/Assets/Scripts/Beat/SetMaterialFloatOnBeat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMaterialFloatOnBeat : BeatBehaviour
+{
+    [SerializeField] Material materialToEffect;
+    [SerializeField] string variableReferenceName;
+
+
+    protected override void OnBeatRangeStay()
+    {
+        materialToEffect.SetFloat(variableReferenceName, beatRangeCloseness);
+    }
+
+    protected override void OnBeatRangeExit()
+    {
+        materialToEffect.SetFloat(variableReferenceName, 0);
+    }
+}
