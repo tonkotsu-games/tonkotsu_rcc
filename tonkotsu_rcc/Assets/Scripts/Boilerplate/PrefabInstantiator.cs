f454dae66c26e8852af5a664fb23b5ba35211621
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-11-10 19:54:31 +0100

    Prefabs can now be Automatically generated when placed in "Resources/AutoPrefabs" Added CheatGUI to AutoPrefabs

---
+++ tonkotsu_rcc/Assets/Scripts/Boilerplate/PrefabInstantiator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PrefabInstantiator
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    public static void RuntimeInit()
+    {
+        Debug.Log("Instantiating AutoPrefabs");
+        InstantiateAutoPrefabs();
+    }
+
+    private static void InstantiateAutoPrefabs()
+    {
+        var gos = Resources.LoadAll<GameObject>("AutoPrefabs/");
+
+        foreach (var prefab in gos)
+        {
+            var g = MonoBehaviour.Instantiate(prefab);
+            g.name = "_AUTO_" + prefab.name;
+            g.hideFlags = HideFlags.DontSaveInBuild | HideFlags.DontSaveInEditor;
+        }
+    }
+}
