57e574a872927330231a5ed1414e1e160c4fe100
Benjamin Grill <benjamin.grill@ue-germany.de>
2019-11-14 11:51:24 +0100

    BehaviorTree

---
+++ tonkotsu_rcc/Assets/Scripts/BehaviorTree/Sequence.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+public class Sequence : BaseNode
+{
+    List<BaseNode> nodes = new List<BaseNode>();
+
+    public Sequence(List<BaseNode> nodes)
+    {
+        this.nodes = nodes;
+    }
+
+    public override NodeState Evaluate()
+    {
+        bool childRunning = false;
+
+        foreach (BaseNode node in nodes)
+        {
+            switch (node.Evaluate())
+            {
+                case NodeState.FAILURE:
+                    {
+                        currentNodeState = NodeState.FAILURE;
+                        return currentNodeState;
+                    }
+                case NodeState.SUCCESS:
+                    {
+                        continue;
+                    }
+                case NodeState.RUNNING:
+                    {
+                        childRunning = true;
+                        continue;
+                    }
+                default:
+                    {
+                        currentNodeState = NodeState.SUCCESS;
+                        return currentNodeState;
+                    }
+            }
+        }
+        currentNodeState = childRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+        return currentNodeState;
+    }
+}
