e0eb583fba8b644123df3440a24f047775636729
Benjamin Rose <rose.benjamin@t-online.de>
2019-11-18 17:21:28 +0100

    tried to find solution to hierarchyIcons

--- tonkotsu_rcc/Assets/Scripts/Editor/HierarchyIcons.cs
+++ tonkotsu_rcc/Assets/Scripts/Editor/HierarchyIcons.cs
@@ -18,38 +18,47 @@ public class HierarchyIcons
         // Init
         referenceTexture = AssetDatabase.LoadAssetAtPath("Assets/Textures/ScriptingTextures/ExclamationMark.png", typeof(Texture2D)) as Texture2D;
         balanceTexture = AssetDatabase.LoadAssetAtPath("Assets/Textures/ScriptingTextures/1200px-Question_mark_grey.svg.png", typeof(Texture2D)) as Texture2D;
-        EditorApplication.hierarchyWindowItemOnGUI += HierarchyItemCB;
-        markedObjects = new List<int>();
+        EditorApplication.hierarchyWindowItemOnGUI += HierarchyIconCB;
+        EditorApplication.update += UpdateCB;
     }
 
-    static void HierarchyItemCB(int instanceID, Rect selectionRect)
+    static void UpdateCB()
     {
-        var go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
-        if (go)
+        Debug.LogError("Update CB");
+
+        markedObjects = new List<int>();
+
+        var foundGameObjects = Object.FindObjectsOfType (typeof(GameObject)) as GameObject[];
+        Debug.Log("found go: " + foundGameObjects.Length);
+        if (foundGameObjects.Length != 0)
         {
-            var components = go.GetComponents<Component>();
-            for (int i = 0; i < components.Length; i++)
+            foreach(GameObject go in foundGameObjects)
             {
-                FieldInfo[] fieldsRequired = GetAllRequired(components, i);
-
-                FieldInfo[] fieldsSerializeField = GetAllSerializedFields(fieldsRequired);
-
-                bool componentHasAllRef = true;
-
-                for (int j = 0; j < fieldsSerializeField.Length; j++)
+                var components = go.GetComponents<Component>();
+                for (int i = 0; i < components.Length; i++)
                 {
-                    if(fieldsSerializeField[j] != null)
+                    FieldInfo[] fieldsRequired = GetAllRequired(components, i);
+
+                    FieldInfo[] fieldsSerializeField = GetAllSerializedFields(fieldsRequired);
+                    Debug.Log("fields " + fieldsSerializeField.Length);
+                    bool componentHasAllRef = true;
+
+                    for (int j = 0; j < fieldsSerializeField.Length; j++)
                     {
-                        if (fieldsSerializeField[j].GetValue(components[i]) == null)
+                        if(fieldsSerializeField[j] != null)
                         {
-                            componentHasAllRef = false;
-                        }
-                    }                   
-                }
-                if (componentHasAllRef == false)
-                {
-                    DrawIcon(selectionRect);
-                }
+                            if (fieldsSerializeField[j].GetValue(components[i]) == null)
+                            {
+                                componentHasAllRef = false;
+                            }
+                        }                   
+                    }
+                    if (componentHasAllRef == false)
+                    {
+                        markedObjects.Add(go.GetInstanceID());
+                    }
+                }           
+                Debug.Log(markedObjects.Count);
             }
         }
     }
@@ -75,12 +84,19 @@ public class HierarchyIcons
         return foundFields;
     }
 
-    private static void DrawIcon(Rect selectionRect)
+    static void HierarchyIconCB(int instanceID, Rect selectionRect)
     {
+
         // place the icoon to the right of the list:
         Rect r = new Rect(selectionRect);
         r.width = 20;
         r.x = r.width - 20;
-        GUI.Label(r, referenceTexture);
+
+        //Debug.LogError("HierarchyIconCB");
+        //Debug.Log("markedObjects.Count: " + markedObjects.Count);
+        if (markedObjects.Contains (instanceID)) 
+        {
+            GUI.Label(r, referenceTexture);
+        }      
     }
 }
