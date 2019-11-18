f709014da7364f76de2d54b0060d9a32abe88c85
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-11-18 12:20:52 +0100

    started work on player controller

---
+++ tonkotsu_rcc/Assets/Scripts/Prototype/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BeatBehaviour
+{
+
+    Rigidbody rigidbody;
+
+    [SerializeField] float velocity;
+    [SerializeField] VirtualController virtualController;
+
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+       var input =  virtualController.GetPackage();
+
+
+        UpdateMovement(input.LeftStick);
+
+
+
+    }
+
+
+    private void UpdateMovement(Vector2 input)
+    {
+
+
+
+    }
+
+
+}
