88dd62222285f3e0fec2b69f04fa999457d61901
Luca Martinelli <lucaandre.martinelli@ue-germany.de>
2019-11-19 19:27:27 +0100

    you can teleport to checkpoints in the inspector now

--- tonkotsu_rcc/Assets/Scripts/Prototype/Checkpoint.cs
+++ tonkotsu_rcc/Assets/Scripts/Prototype/Checkpoint.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class Checkpoint : MonoBehaviour
 {
-    
+    [Button]
     [CheatMethod]
     public void TeleportPlayer()
     {
